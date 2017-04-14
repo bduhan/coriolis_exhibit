647a2aa79c6fb323f1cefac453501d6be310e47e
Brian Duhan <Brian Duhan>
2017-04-14 15:19:48 -0500

    Latest code, exhibit is in testing with this.

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -21,7 +21,7 @@ unsigned long run_time = 8000;
 #define BTN_DECREASE 37 // active LOW
 #define BTN_USER 31 // unknown
 #define LED_PIN 13 // not really used
-#define BTN_INTERVAL 5 // button check interval in ms
+#define BTN_INTERVAL 25 // button check interval in ms
 
 // upper motor driver
 #define UM_STEP 51
@@ -35,21 +35,25 @@ unsigned long run_time = 8000;
 // how much to increment or decrement (steps)
 #define INC_AMT 5
 
-float MAX_SPEED = 1000;
-float MIN_SPEED = 10;
-
 /* TODO
 motor speeds probably aren't going to be the same
 we will need to characterize the differences in turn rates
 also, this is probably where the encoder would be most useful
+
+upper driver was set to all defaults (all switches up)
+changing sw6 to down seemed to make it smooth but also I was stepping too fast
 */
 
 
 /* END OF DEFINITIONS */
 unsigned long run_start;
 
-int run_speed = 100;
-bool running = false;
+int run_speed = 1300; // steps per second
+int accel = 800; // steps per second
+unsigned long target_position = 128000; // position to move to when user presses button 
+bool running = false; // are we running right now?
+float multiplier = .25; //.4905; // lower motor targets
+int align_offset_steps = 5; // how much to adjust up or down in steps for lower.  for visual alignment
 Bounce increase = Bounce();
 Bounce decrease = Bounce();
 Bounce user = Bounce();
@@ -57,13 +61,17 @@ Bounce user = Bounce();
 AccelStepper upper(AccelStepper::DRIVER, UM_STEP, UM_DIR);
 AccelStepper lower(AccelStepper::DRIVER, LM_STEP, LM_DIR);
 
+MultiStepper steppers;
+
 void setup() {
-  upper.setMaxSpeed(MAX_SPEED);
-  lower.setMaxSpeed(MAX_SPEED);
+  //upper.setMaxSpeed(MAX_SPEED);
+  //lower.setMaxSpeed(MAX_SPEED);
 
   // set and invert enable pin
   upper.setEnablePin(UM_EN);
-  upper.setPinsInverted(false, false, true);
+  //upper.setPinsInverted(false, false, true);
+  upper.setPinsInverted(true, false, true);
+  lower.setPinsInverted(true, false, false);
 
   // buttons
   pinMode(BTN_INCREASE, INPUT_PULLUP);
@@ -78,62 +86,62 @@ void setup() {
   
   Serial.begin(9600);
   Serial.println("setup!");
-  upper.setSpeed(0);
-  upper.setAcceleration(10);
-  lower.setSpeed(0);
-  lower.setAcceleration(10);
+
+  upper.setMaxSpeed(run_speed);
+  upper.setAcceleration(accel);
+  lower.setMaxSpeed(run_speed * multiplier);
+  lower.setAcceleration(accel * multiplier);
+
+  steppers.addStepper(upper);
+  steppers.addStepper(lower);
+
+  //upper.moveTo(4000);
+  //lower.moveTo(1940);
+  /*
+  long positions[2];
+  positions[0] = target_position;
+  positions[1] = target_position * multiplier;
+  steppers.moveTo(positions);
+  */
 }
 
 void loop() {
+  user.update();
   increase.update();
   decrease.update();
-  user.update();
 
-  if (running == true) {
-    unsigned long rt = run_start + run_time;
-    //Serial.println(rt);
-    if (rt < millis()) {
-      Serial.println("STOP!");
-      running = false;
-      run_start = 0;
-      upper.setSpeed(0.0);
-      lower.setSpeed(0.0);
-      digitalWrite(LED_PIN, LOW);
-    }
-  } else {
-    if (user.read() == LOW) {
-      Serial.print("RUN speed: ");
-      Serial.println(run_speed);
-      running = true;
-      run_start = millis();
-      upper.setSpeed(run_speed);
-      lower.setSpeed(run_speed);
+  //steppers.run();
+  if ((upper.isRunning() & lower.isRunning()) == false)
+    running = false; 
+
+  if (user.read() == LOW && running == false) {
+    long positions[2];
+    upper.setCurrentPosition(0);
+    lower.setCurrentPosition(0);
+    positions[0] = target_position;
+    positions[1] = target_position * multiplier;
+    steppers.moveTo(positions);
+    running = true;
+    Serial.println("run");
+  }
+
+  if (running == false) {
+    if ((increase.read() == LOW || decrease.read() == LOW)) {
+      int offset = 0;
+      if (increase.read() == LOW) {
+        offset = align_offset_steps;
+      }
+      else if (decrease.read() == LOW) {
+        offset = align_offset_steps * -1;
+      }
+      lower.moveTo(lower.currentPosition() + offset);
+      //Serial.print("Offsetting lower: ");
+      //Serial.println(offset);
+      lower.runToPosition();
+      //Serial.println("done!");
     }
   }
 
-  // increment/decrement speed from buttons
-  if (increase.read() == LOW) {
-    if (run_speed + INC_AMT < MAX_SPEED) {
-      Serial.print("INcrease speed to: ");
-      Serial.println(run_speed);
-      run_speed += INC_AMT;
-    } else {
-      Serial.println("AT MAX SPEED");
-    }
-  }
-
-  if (decrease.read() == LOW) {
-    if (run_speed - INC_AMT > MIN_SPEED) {
-      Serial.print("DEcrease speed to: ");
-      Serial.println(run_speed);
-      run_speed -= INC_AMT;
-    } else {
-      Serial.println("AT MIN SPEED");
-    }
-  }
-
-  //upper.runSpeed();
-  //lower.runSpeed();
-  upper.runSpeed();
-  lower.runSpeed();
+  upper.run();
+  lower.run();
 }
